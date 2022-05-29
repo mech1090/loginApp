8c956e344c56f2e4dcf39387f4d28be055ba9107
Shahzad <mech1090@gmail.com>
2022-05-29 06:48:59 -0600

    routes and controller setup completed

---
+++ controller/user.controller.js
@@ -0,0 +1,10 @@
+
+
+const getLoginForm = (req,res)=>{
+    res.send('Hitting routes and controllers')
+}
+const login = (req,res)=>{}
+const getSignupForm = (req,res)=>{}
+const signup = (req,res)=>{}
+
+module.exports = {getLoginForm,login,getSignupForm,signup}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const config = require('config')
 const { default: mongoose } = require('mongoose')
+const userPage = require('./routes/user')
 require('./db')
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
+app.use('/user',userPage)
 
 app.get('/',(req,res)=>{
     res.send('initial set up okk')
